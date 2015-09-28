b2b89af7fb7eeea1e10b00d18dcd5fb412c5fc56
tryinggithubnow <qureshi.anees82@gmail.com>
2015-09-28 15:57:31 +0530

    Update JavaHelloWorld.java

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,7 +2,7 @@ public class JavaHelloWorld
 	{
 		public static void main (String [] args)
 			{
-				System.Out.Println("Java Hello World");
-				System.Out.Println("Hello Again");
+				System.out.println("Java Hello World");
+				System.out.println("Hello Again");
 			}
 	}
